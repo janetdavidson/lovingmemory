0099d827f9d6c6c16adbd809baee0b4ea9faae9f
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2021-06-11 17:29:43 -0700

    Changed wording, Uncapitalized, removed covid

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -5,7 +5,6 @@
     </div>
     <div class="section details">
       <h1 class="title">Service Livestream</h1>
-      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Church address then at Crematorium address.</p>
       <h2>Memorial Service</h2>
       <p>The memorial service will be livestreamed at Cedar City Stake Center.
       <p>There will be no live stream at the Cemetery.
