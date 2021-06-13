fff8d135b6447acd3a04eac43d68e251dd4efeda
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2021-06-12 20:47:54 -0700

    added bold, added icon

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section details section-70">
       <h1 class="title">Memorial Service Livestream</h1>
-      <p>We hope those that can't make it in person will join us virtually. The services will be held on <em>Wednesday, June, 16, 2021</em>.</p>
+      <p>We hope those that can't make it in person will join us virtually. The services will be held on <span class="bold">Wednesday, June, 16, 2021</span>.</p>
       <h2>Memorial Service</h2>
       <p>The memorial service will be livestreamed at Cedar City Stake Center.
       <p>There will be no livestream at the Cemetery.
--- pages/obituary.vue
+++ pages/obituary.vue
@@ -24,7 +24,7 @@
         The family would like to express their deepest gratitude to the doctors and nurses, the hospice workers, and the wonderful members of the East Bench Ward for their tender loving care of Jan while she went through three rounds of intense cancer treatments over the last two years.
       </div>
       <div class="text">
-        Online condolences can be added to <a target="_blank" href="https://memorials.affordablefuneralservices.com/janet-davidson/4642107/obituary.php">Affordable Funeral Services' webpage for Janet Davidson</a> by clicking the Memory Wall tab, located underneath Jan's photo.
+        Online condolences can be added to <a class="same-line" target="_blank" href="https://memorials.affordablefuneralservices.com/janet-davidson/4642107/obituary.php"><i class="fas fa-external-link-alt"></i> Affordable Funeral Services' webpage for Janet Davidson</a> by clicking the Memory Wall tab, located underneath Jan's photo.
       </div>
       <div class="image">
         <img src="~/assets/Jack and Janet - mortuary memorial service.jpg" alt="In loving memory" />
