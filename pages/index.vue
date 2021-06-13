2d0fb6a997eed42c34a74045dbcfe1fc57593f59
Kristen Davidson <kristendavidson@gmail.com>
2021-06-12 20:06:22 -0600

    added obit pic

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section tribute">
       <h1 class="title">In Loving Memory</h1>
-      <img height="370" src="~/assets/frontimage.jpg" alt="In loving memory" />
+      <img height="370" src="~/elements/Janet Davidson - obituary.jpg" alt="In loving memory" />
       <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
       <a class="button" target="_blank" rel=”noopener” href="https://memorials.affordablefuneralservices.com/janet-davidson/4642107/obituary.php">Online Condolences</a>
     </div>
