b7548b992ac64f1c8558fbbf1bef685cd0f81159
janetdavidson <janetdavidson@users.noreply.github.com>
2021-06-16 14:46:28 +0000

    deploy: 4e7b6db0eda380a754c2111e7158335048519495

--- _nuxt/static/1623853888/gallery/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623853888",layout:"default",error:null,serverRendered:true,routePath:"\u002Fgallery",config:{name:"Janet (Anderson) Davidson",dates:"Thursday, July 1st 1948 - Wednesday, June 9th 2021"}};
