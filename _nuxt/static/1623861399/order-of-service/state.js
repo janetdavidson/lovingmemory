acd2055c0c384e7e4bf97f2124384db47b1fa99f
janetdavidson <janetdavidson@users.noreply.github.com>
2021-06-16 16:36:45 +0000

    deploy: 28bebb15912d6e028fdf612fd5f145506abcdbc1

---
+++ _nuxt/static/1623861399/order-of-service/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623861399",layout:"default",error:null,serverRendered:true,routePath:"\u002Forder-of-service",config:{name:"Janet (Anderson) Davidson",dates:"Thursday, July 1st 1948 - Wednesday, June 9th 2021"}};
