e9c2fbc14c2a677a1559bd78a3334a0d3640f640
Kristen Davidson <kristendavidson@gmail.com>
2021-06-20 23:47:50 -0600

    changed order of service, removed livestream

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -4,7 +4,7 @@
       <Nav />
     </div>
     <div class="section details section-70">
-      <h1 class="title">Order of Service <div style="position: relative;width: 108px;height:30px; display: inline-block;">
+      <h1 class="title">Video and Program - Memorial Service <div style="position: relative;width: 108px;height:30px; display: inline-block;">
         <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fjanetdavidson.org&width=108&layout=button_count&action=like&size=large&share=false&height=21&appId=1127785873984291" width="108" height="30" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
       </div></h1>
       <p><em>Wednesday, June 16, 2021</em></p>
@@ -18,12 +18,9 @@
             </a>
           </div>
           <ul class="fa-ul">
-            <ListItem>Service starts at 11:00 a.m. at <a class="same-line" target="_blank" href="https://www.google.com/maps/dir//Cedar+Stake+Center,+155+E+400+S,+Cedar+City,+UT+84720/@37.6733592,-113.0790912,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x80b561c846b42805:0x7f299b75c56ace35!2m2!1d-113.0588829!2d37.6704444"><i class="fas fa-map-marker-alt"></i> Cedar City Stake Center</a></ListItem>
-            <ListItem>Welcome &amp; Conducting (Bishop Brady Smith)</ListItem>
-            <ListItem>Opening Prayer (Lorraine Davidson)</ListItem>
-            <ListItem>Remembrances - Family &amp; Friends</ListItem>
-            <ListItem>Remarks (Bishop Brady Smith)</ListItem>
-            <ListItem>Closing prayer (Kristen Davidson)</ListItem>
+            <ListItem>Janet's Memorial Service was held on Wednesday, June 16, at her church building. The service was available via zoom, and was recorded. You can view a recording of the service, as well as the Program below.
+            Her Inurnment was held at the Cedar City Cemetery Columbarium and was not livestreamed. The Columbrarium is located just behind the Cemetery Admin Offices, down a flowered path. Her location is on the opposite side of the path, facing N 400 W Street.</ListItem>
+
           </ul>
         </div>
         <div class="program">
@@ -34,14 +31,8 @@
           </div>
         </div>
       </div>
-      <div class="map">
-        <iframe src="https://maps.google.com/maps?q=155 E 400 S Cedar City, Utah&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"/>
-      </div>
       <h2>Cedar City Cemetery <span>2:00pm</span></h2>
-      <ul class="fa-ul">
-        <ListItem>Service starts at 2:00 p.m. at <a class="same-line" target="_blank" href="https://www.google.com/maps/dir//37.6895447,-113.0644744/@37.6897781,-113.0647963,19z"><i class="fas fa-map-marker-alt"></i> Cedar City Cemetery Columbarium</a></ListItem>
-        <ListItem>Dedicatory Prayer (Jack L Davidson)</ListItem>
-      </ul>
+
       <div class="map">
         <iframe src="https://maps.google.com/maps?q=cedar%20city%20cemetery&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"/>
       </div>
