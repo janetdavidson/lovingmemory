30d6a3aa45f5d5007488ddb1222eebb2c2c64fdc
janetdavidson <85757593+janetdavidson@users.noreply.github.com>
2021-06-23 13:46:09 -0700

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -7,11 +7,11 @@
       <h1 class="title">Video and Program - Memorial Service <div style="position: relative;width: 108px;height:30px; display: inline-block;">
         <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fjanetdavidson.org&width=108&layout=button_count&action=like&size=large&share=false&height=21&appId=1127785873984291" width="108" height="30" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
       </div></h1>
-      <p><em>Wednesday, June 16, 2021</em></p>
+      <p><em>Recorded: Wednesday, June 16, 2021</em></p>
 
       <div style="width:100%;display:flex;">
         <div class="memorial">
-          <h2>Memorial Service <span>11:00am</span></h2>
+          <h2>Memorial Service</h2>
           <div class="program-small">
             <a href="/layout/images/memorial service program.jpg" target="_blank">
               <img height="250" src="/layout/images/memorial service program.jpg" alt="Click to view larger"/>
@@ -31,7 +31,7 @@
           </div>
         </div>
       </div>
-      <h2>Cedar City Cemetery <span>2:00pm</span></h2>
+      <h2>Cedar City Cemetery</h2>
 
       <div class="map">
         <iframe src="https://maps.google.com/maps?q=cedar%20city%20cemetery&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"/>
